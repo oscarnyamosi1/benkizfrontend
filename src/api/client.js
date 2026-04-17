54231401e6ba6f6b0565e66a84504aea119f4add
malik taylor l-mint <oscarnyamosi2@gmail.com>
2026-04-17 23:34:32 +0300

    changed base url to (https://benkizbakers.pythonanywhere.com/) from local (http://127.0.0.1:8000/api)

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://benkizbakers.pythonanywhere.com/api',
   withCredentials: true,
   headers: { 'Content-Type': 'application/json' },
 })
